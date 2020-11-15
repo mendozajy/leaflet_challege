a69cf92a5b2085470ea70c6856b5fa652e8d41ea
Jessica Mendoza <mendoza.jy@gmail.com>
2020-11-14 19:59:51 -0800

    adding final changes for homework

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # leaflet_challege
 Homework for leaflet and mapping
-Contains static file, index.html file, 
+Contains static file, index.html file
+
+referenced
+https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+
+
--- static/js/logic.js
+++ static/js/logic.js
@@ -17,3 +17,93 @@ var myMap = L.map("mapid", {
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   }).addTo(myMap);
+
+// This function will return the hex color for our circle depending on the size of the earthquake
+
+function plotcolor(earthquakesize) {
+  switch (true) {
+      case earthquakesize > 5:
+          return "#581845";
+      case earthquakesize > 4:
+          return "#900C3F";
+      case earthquakesize > 3:
+          return "#C70039";
+      case earthquakesize > 2:
+          return "#FF5733";
+      case earthquakesize > 1:
+          return "#FFC300";
+      default:
+          return "#DAF7A6";
+      }
+}
+// This function will determine the size of each circle
+
+function markerSize(earthquakesize) {
+  if (earthquakesize = 0) {
+    return 1;
+  }
+  return earthquakesize * 30000;
+}
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+
+  // loop through each earthquake
+
+  for (var i = 0; i < data.features.length; i++) {
+      
+      // save the magnitude of each quake
+      earthquakesize=data.features[i].properties.mag
+      earthquakesize= +earthquakesize
+
+      // save the coordinates of each quake and convert them to numbers
+      cords= data.features[i].geometry.coordinates
+
+      cords[0] = +cords[0];
+      cords[1] = +cords[1];
+      cords[2] = +cords[2];
+
+      // Determine the size of the circle
+
+      marker = markerSize(earthquakesize)
+      marker = +marker
+
+      // plot the circle
+      var circle = L.circle([cords[1], cords[0], cords[2]], {
+          fillOpacity: 0.75,
+          color:  plotcolor(earthquakesize),
+          radius: marker
+         })
+
+      // Add Info For when someone clicks on a circle   
+      circle.bindPopup(
+          "<h3>" + "Earthquake Location: " + data.features[i].properties.place +
+          "</h3><hr><p>" + "Occured: " + new Date(data.features[i].properties.time) + "</p>" +
+          "</h3><br><p>" + "Earthquake Magnitude: " + earthquakesize + "</p>" 
+             ).addTo(myMap);
+             
+  }
+});
+// Add legend to our map
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        magnitude = [0,1,2,3,4,5]
+
+        div.innerHTML +=
+            '<i style="background:' + plotcolor(magnitude[0] + 1) + '"></i> ' +
+           "< " + magnitude[0] + (magnitude[0 + 1]  ? '&ndash;' + magnitude[0 + 1] + '<br>' : '+')
+
+    for (var i = 1; i < magnitude.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + plotcolor(magnitude[i] + 1) + '"></i> ' +
+            magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
